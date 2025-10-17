6857eeb37b3db201ed4c1c783fe6749f8490009c
supperclock <supperclock@126.com>
2025-10-17 15:04:45 +0800

    111

--- kimi/python-v/kimi_backend.py
+++ kimi/python-v/kimi_backend.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import os
-from ai import check_game_over
-from py_xq_wrapper import minimax_root
+from ai import check_game_over,minimax_root
 from util import log
 # from ai_bridge import find_best_move_c
 from ai_gpt_bridge import find_best_move_c
